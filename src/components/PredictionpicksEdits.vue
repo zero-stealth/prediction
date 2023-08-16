0611422977b0bce97c667e46ec82a2b9d5c472e3
zero-stealth <penguincipher@gmail.com>
2023-08-16 12:54:38 +0300

    stable

--- src/components/PredictionpicksEdits.vue
+++ src/components/PredictionpicksEdits.vue
@@ -8,15 +8,33 @@
         <h1>Team A</h1>
         <div class="form-group">
           <label for="teamA">Name:</label>
-          <input v-model="teamA" type="text" class="form-g-input" placeholder="Manchester" id="teamA" />
+          <input
+            v-model="teamA"
+            type="text"
+            class="form-g-input"
+            placeholder="Manchester"
+            id="teamA"
+          />
         </div>
         <div class="form-group">
           <label for="teamAIcon">Logo:</label>
-          <input @change="handleTeamALogo" type="file" class="form-g-input" id="teamAIcon" accept="image/*" />
+          <input
+            @change="handleTeamALogo"
+            type="file"
+            class="form-g-input"
+            id="teamAIcon"
+            accept="image/*"
+          />
         </div>
         <div class="form-group">
           <label for="formationA">Formation:</label>
-          <input v-model="formationA" type="text" class="form-g-input" placeholder="l-w-d-w" id="formationA" />
+          <input
+            v-model="formationA"
+            type="text"
+            class="form-g-input"
+            placeholder="l-w-d-w"
+            id="formationA"
+          />
         </div>
         <div class="form-group">
           <label for="Status">Status:</label>
@@ -24,11 +42,23 @@
         </div>
         <div class="form-group">
           <label for="teamAPosition">Position:</label>
-          <input v-model="teamAPosition" type="text" class="form-g-input" placeholder="1" id="teamAPosition" />
+          <input
+            v-model="teamAPosition"
+            type="text"
+            class="form-g-input"
+            placeholder="1"
+            id="teamAPosition"
+          />
         </div>
         <div class="form-group">
           <label for="teamAscore">Score:</label>
-          <input v-model="teamAscore" type="text" class="form-g-input" placeholder="2" id="teamAscore" />
+          <input
+            v-model="teamAscore"
+            type="text"
+            class="form-g-input"
+            placeholder="2"
+            id="teamAscore"
+          />
         </div>
       </div>
       <div class="form-wrapper">
@@ -38,11 +68,23 @@
         </div>
         <div class="form-group">
           <label for="leagueIcon">League logo</label>
-          <input @change="handleLeagueLogo" type="file" class="form-g-input" id="leagueIcon" accept="image/*" />
+          <input
+            @change="handleLeagueLogo"
+            type="file"
+            class="form-g-input"
+            id="leagueIcon"
+            accept="image/*"
+          />
         </div>
         <div class="form-group">
           <label for="league">Match league:</label>
-          <input v-model="league" type="text" class="form-g-input" placeholder="team league" id="league" />
+          <input
+            v-model="league"
+            type="text"
+            class="form-g-input"
+            placeholder="team league"
+            id="league"
+          />
         </div>
         <div class="form-group">
           <label for="time">Match Time:</label>
@@ -50,35 +92,77 @@
         </div>
         <div class="form-group">
           <label for="date">Match Date:</label>
-          <input v-model="date" type="text" class="form-g-input" placeholder="03-06-2023" id="date" />
+          <input
+            v-model="date"
+            type="text"
+            class="form-g-input"
+            placeholder="03-06-2023"
+            id="date"
+          />
         </div>
         <div class="form-group">
           <label for="score">Show score:</label>
-          <input v-model="ShowScore" type="text" class="form-g-input" placeholder="true" id="score" />
+          <input
+            v-model="ShowScore"
+            type="text"
+            class="form-g-input"
+            placeholder="true"
+            id="score"
+          />
         </div>
-      <button type="submit" class="btn-f-f f-desktop">Submit</button>
+        <button type="submit" class="btn-f-f f-desktop">Submit</button>
       </div>
       <div class="form-wrapper">
         <h1>Team B</h1>
         <div class="form-group">
           <label for="teamB">Name:</label>
-          <input v-model="teamB" type="text" class="form-g-input" placeholder="Arsenal" id="teamB" />
+          <input
+            v-model="teamB"
+            type="text"
+            class="form-g-input"
+            placeholder="Arsenal"
+            id="teamB"
+          />
         </div>
         <div class="form-group">
           <label for="teamBIcon">Logo:</label>
-          <input @change="handleTeamBLogo" type="file" class="form-g-input" id="teamBIcon" accept="image/*" />
+          <input
+            @change="handleTeamBLogo"
+            type="file"
+            class="form-g-input"
+            id="teamBIcon"
+            accept="image/*"
+          />
         </div>
         <div class="form-group">
           <label for="formationB">Form:</label>
-          <input v-model="formationB" type="text" class="form-g-input" placeholder="l-w-d-w" id="formationB" />
+          <input
+            v-model="formationB"
+            type="text"
+            class="form-g-input"
+            placeholder="l-w-d-w"
+            id="formationB"
+          />
         </div>
         <div class="form-group">
           <label for="teamBPosition">Position:</label>
-          <input v-model="teamBPosition" type="text" class="form-g-input" placeholder="2" id="teamBPosition" />
+          <input
+            v-model="teamBPosition"
+            type="text"
+            class="form-g-input"
+            placeholder="2"
+            id="teamBPosition"
+          />
         </div>
         <div class="form-group">
           <label for="teamBscore">Score:</label>
-          <input v-model="teamBscore" type="text" class="form-g-input" placeholder="5" id="teamBscore" />
+          <input
+            v-model="teamBscore"
+            type="text"
+            class="form-g-input"
+            placeholder="5"
+            id="teamBscore"
+          />
         </div>
         <button type="submit" class="btn-f-f f-mobile">Submit</button>
       </div>
@@ -87,45 +171,44 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
 const emit = defineEmits(['formSubmit'])
 
-
-const teamA = ref('');
-const teamB = ref('');
-const teamAIcon = ref('');
-const teamBIcon = ref('');
-const leagueIcon = ref('');
-const formationA = ref('');
-const formationB = ref('');
-const teamAPosition = ref('');
-const teamBPosition = ref('');
-const time = ref('');
-const status = ref('');
-const league = ref('');
-const teamAscore = ref('');
-const teamBscore = ref('');
-const date = ref('');
-const tip = ref('');
-const ShowScore = ref(false);
+const teamA = ref('')
+const teamB = ref('')
+const teamAIcon = ref('')
+const teamBIcon = ref('')
+const leagueIcon = ref('')
+const formationA = ref('')
+const formationB = ref('')
+const teamAPosition = ref('')
+const teamBPosition = ref('')
+const time = ref('')
+const status = ref('')
+const league = ref('')
+const teamAscore = ref('')
+const teamBscore = ref('')
+const date = ref('')
+const tip = ref('')
+const ShowScore = ref(false)
 
 function handleFileUpload(event, targetRef) {
-  const file = event.target.files[0];
+  const file = event.target.files[0]
   if (file) {
-    targetRef.value = file;
+    targetRef.value = file
   }
 }
 
 function handleTeamALogo(event) {
-  handleFileUpload(event, teamAIcon);
+  handleFileUpload(event, teamAIcon)
 }
 
 function handleTeamBLogo(event) {
-  handleFileUpload(event, teamBIcon);
+  handleFileUpload(event, teamBIcon)
 }
 
 function handleLeagueLogo(event) {
-  handleFileUpload(event, leagueIcon);
+  handleFileUpload(event, leagueIcon)
 }
 
 // ... (your existing code)
@@ -151,11 +234,11 @@ function handleSubmit() {
       teamBPosition: teamBPosition.value,
       teamAIcon: teamAIcon.value,
       teamBIcon: teamBIcon.value
-    };
+    }
 
-    emit('formSubmit', formData);
+    emit('formSubmit', formData)
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
 }
 </script>
