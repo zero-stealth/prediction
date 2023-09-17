790c41129d7c84f7678ec356cac140ea6510297a
zero-stealth <penguincipher@gmail.com>
2023-09-17 04:43:46 +0300

    stable

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'Home',
       component: HomeView,
       meta: {
