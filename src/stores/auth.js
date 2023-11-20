c6582be86768db0ce2c5518f439fc50e3bd2f4ac
zero-stealth <penguincipher@gmail.com>
2023-11-21 01:28:44 +0300

    'stable

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -3,18 +3,16 @@ import { defineStore } from "pinia";
 
 export const useAuthStore = defineStore("auth", () => {
   const state = {
-    token : ref(null)
+    token: ref(localStorage.getItem('token') || null)
   };
-
-  
 
   const toggleToken = (token) => {
     state.token.value = token;
+    localStorage.setItem('token', token);
   };
 
-
   return {
-    ...state, 
+    ...state,
     toggleToken,
   };
 });
