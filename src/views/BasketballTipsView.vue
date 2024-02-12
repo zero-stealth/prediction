652474642f745755b5537e2aa9dd88ebc0dddae8
zero-stealth <penguincipher@gmail.com>
2024-02-12 23:27:34 +0300

    stable

--- src/views/BasketballTipsView.vue
+++ src/views/BasketballTipsView.vue
@@ -1,9 +1,9 @@
 <script setup>
 import axios from 'axios'
-import { useRoute, useRouter } from 'vue-router'
-import banner from '@/assets/banner.jpeg'
+import { DateTime } from 'luxon'
 import ArrowIcon from '@/icons/ArrowIcon.vue'
-import { onMounted, ref, shallowRef } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { ref, computed, onMounted, shallowRef } from 'vue'
 import AdsTipComponent from '../components/adsTipComponent.vue'
 // pages
 import formationSection from '../components/basketball/formationSection.vue'
@@ -18,11 +18,16 @@ const route = useRoute()
 const teamA = ref('')
 const teamB = ref('')
 const league = ref('')
+const cardAdsData = ref([])
+const formationsA = ref([])
+const formationsB = ref([])
 const teamAscore = ref('')
 const teamBscore = ref('')
 const teamAIcon = ref('')
 const teamBIcon = ref('')
 const leagueIcon = ref('')
+const cardAdsImg = ref(null)
+const cardAdsLink = ref(null)
 const teamAPosition = ref('')
 const teamBPosition = ref('')
 const time = ref('')
@@ -39,6 +44,26 @@ const getButtonClass = (page) => {
   return ['nav-tip', activePage.value === page ? 'active-tip' : 'inactive-tip']
 }
 
+const getAds = async () => {
+  try {
+    const response = await axios.get(`${SERVER_HOST}/ads`)
+    cardAdsData.value = response.data
+    showAds()
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const filteredAds = computed(() => {
+  const AdTitle = 'Card'
+  return cardAdsData.value ? cardAdsData.value.filter((Ads) => Ads.title === AdTitle) : []
+})
+
+const showAds = () => {
+  cardAdsImg.value = filteredAds.value[0]?.description || null
+  cardAdsLink.value = filteredAds.value[0]?.link || null
+}
+
 async function getTip() {
   const response = await axios.get(
     `${SERVER_HOST}/sports/single/${route.params.date}/${route.params.teamA}/${route.params.teamB}`
@@ -51,11 +76,24 @@ async function getTip() {
   teamAscore.value = response.data.teamAscore
   teamAPosition.value = response.data.teamAPosition
   teamBPosition.value = response.data.teamBPosition
+  formationsA.value = formatFormation(response.data.formationA)
+    ? response.data.formationA[0].split('-')
+    : []
+  formationsB.value = formatFormation(response.data.formationB)
+    ? response.data.formationB[0].split('-')
+    : []
   teamBscore.value = response.data.teamBscore
   league.value = response.data.league
   leagueIcon.value = response.data.leagueIcon
   time.value = response.data.time
   date.value = response.data.date
+}
+
+const formatFormation = (formation) => {
+  if (Array.isArray(formation)) {
+    return formation[0].split('-')
+  }
+  return []
 }
 
 onMounted(() => {
@@ -65,7 +103,6 @@ onMounted(() => {
 const goBack = () => {
   router.go(-1)
 }
-
 
 function formatDate(inputDate) {
   const parts = inputDate.split('-')
@@ -91,65 +128,143 @@ function formatDate(inputDate) {
   const formattedDate = `${day} | ${months[parseInt(month, 10) - 1]} | ${year}`
   return formattedDate
 }
+
+const formattedTime = computed(() => {
+  try {
+    const eventDate = DateTime.fromISO(time.value, { zone: 'utc' })
+
+    const userTimeZone = DateTime.local().zoneName
+
+    const convertedTime = eventDate.setZone(userTimeZone)
+
+    const adjustedTime = convertedTime.minus({ hours: 3 })
+
+    return adjustedTime.toFormat('HH:mm')
+  } catch (error) {
+    console.error('Error formatting time:', error)
+    return time
+  }
+})
+
+onMounted(() => {
+  getAds()
+})
 </script>
 
 <template>
   <div class="detail-apx">
     <div class="apx-byt">
       <div class="details-container">
-      <div class="details-h-conx">
-        <div class="details-header-title">
-          <h1>{{ teamA }} vs {{ teamB }} prediction {{ formatDate(date) }}</h1>
-        </div>
-        <div class="details-h">
-          <ArrowIcon class="details-arrow" @click="goBack()" />
-          <div class="details-h-inn">
-            <img :src="leagueIcon" alt="" class="tbl-f-image tbl-l-i" />
-            <h2>{{ league }}</h2>
-          </div>
-          <span class="pulse"></span>
-        </div>
-      </div>
-      <div
-        class="details-image"
-        :style="{
-          backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.9), #042f59), url(${banner})`
-        }"
-      >
-        <div class="details-image-d">
-          <div class="details-d">
-            <img :src="teamAIcon" alt="" class="details-d-img" />
-            <span>{{ teamA }}</span>
-          </div>
-          <div class="details-midd">
-            {{ [time] }}
-          </div>
-          <div class="details-d">
-            <img :src="teamBIcon" alt="" class="details-d-img" />
-            <span>{{ teamB }}</span>
+        <div class="details-h-conx">
+          <div class="details-header-title">
+            <h1>{{ teamA }} vs {{ teamB }} prediction {{ formatDate(date) }}</h1>
           </div>
         </div>
+
+        <div
+          class="card-container tips-card"
+          :class="[league === '' ? 'card-skeleton' : '', showScore === true ? 'game-played' : '']"
+        >
+            <div class="details-h">
+              <ArrowIcon class="details-arrow" @click="goBack()" />
+              <div class="details-h-inn">
+                <img :src="leagueIcon" alt="" class="tbl-f-image tbl-l-i" />
+                <h2>{{ league }}</h2>
+              </div>
+              <span class="pulse"></span>
+            </div>
+          <div class="card-center">
+            <div class="card-a">
+              <div class="card-fade">
+                <div class="card-inner">
+                  <img
+                    :src="teamAIcon"
+                    alt="card-img"
+                    :class="[
+                      sport === 'Tennis' ? 'card-img circle-tennis' : 'card-img square-img',
+                      teamAIcon === '' ? 'card-img-skeleton' : ''
+                    ]"
+                  />
+                </div>
+              </div>
+              <h2>{{ teamA }}</h2>
+            </div>
+            <div class="card-in">
+              <div class="card-in-s">
+                <span>[{{ formattedTime }}]</span>
+              </div>
+              <span class="status-p">{{ status }}</span>
+              <div v-if="!showScore">
+                <span>vs</span>
+              </div>
+              <div v-if="shouldShowScore" class="card-score">
+                <span class="card-s">{{ teamAscore }}</span>
+                <span class="card-p">:</span>
+                <span class="card-s">{{ teamBscore }}</span>
+              </div>
+              <div v-if="cardAdsImg" class="bet-adv">
+                <a :href="cardAdsLink">
+                  <img :src="cardAdsImg" alt="image" class="bet-winner-logo" />
+                </a>
+              </div>
+            </div>
+            <div class="card-a">
+              <div class="card-fade">
+                <div class="card-inner">
+                  <img
+                    :src="teamBIcon"
+                    alt="card-img"
+                    :class="[
+                      sport === 'Tennis' ? 'card-img circle-tennis' : 'card-img square-img',
+                      teamBIcon === '' ? 'card-img-skeleton' : ''
+                    ]"
+                  />
+                </div>
+              </div>
+              <h2>{{ teamB }}</h2>
+            </div>
+            
+          </div>
+          <div class="card-footer">
+      <div class="card-f" v-for="formationA in formationsA" :key="formationA">
+        <span :class="[formationA === 'l' ? 'loose' : formationA === 'w' ? 'win' : 'draw']">{{
+          formationA
+        }}</span>
       </div>
-      <div class="nav-tips">
-        <button @click="setActivePage(overviewSection)" :class="getButtonClass(overviewSection)">
-          <span>Preview</span>
-        </button>
-        <button @click="setActivePage(tipsSection)" :class="getButtonClass(tipsSection)">
-          <span>Prediction</span>
-        </button>
-        <button @click="setActivePage(standingsSection)" :class="getButtonClass(standingsSection)">
-          <span>Standing</span>
-        </button>
-        <button @click="setActivePage(formationSection)" :class="getButtonClass(formationSection)">
-          <span>H2H</span>
-        </button>
+      <div class="card-fi">Recent form</div>
+      <div class="card-f" v-for="formationB in formationsB" :key="formationB">
+        <span :class="[formationB === 'l' ? 'loose' : formationB === 'w' ? 'win' : 'draw']">{{
+          formationB
+        }}</span>
       </div>
     </div>
-    <div class="tips-main">
-      <component :is="activePage" />
+        </div>
+        <div class="nav-tips">
+          <button @click="setActivePage(overviewSection)" :class="getButtonClass(overviewSection)">
+            <span>Preview</span>
+          </button>
+          <button @click="setActivePage(tipsSection)" :class="getButtonClass(tipsSection)">
+            <span>Prediction</span>
+          </button>
+          <button
+            @click="setActivePage(standingsSection)"
+            :class="getButtonClass(standingsSection)"
+          >
+            <span>Standing</span>
+          </button>
+          <button
+            @click="setActivePage(formationSection)"
+            :class="getButtonClass(formationSection)"
+          >
+            <span>H2H</span>
+          </button>
+        </div>
+      </div>
+      <div class="tips-main">
+        <component :is="activePage" />
+      </div>
     </div>
-    </div>
-  
+
     <div class="apx-pin">
       <AdsTipComponent />
     </div>
