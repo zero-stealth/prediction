bd62b22038bd210c56b64d52dda96a953c851119
zero-stealth <penguincipher@gmail.com>
2024-03-30 15:33:47 +0300

    email extract

--- src/views/VerifyView.vue
+++ src/views/VerifyView.vue
@@ -3,7 +3,6 @@
     <div class="verify-inn">
       <VerifyIcon class="verify-icon" v-if="isVerified" />
       <NotVerifyIcon class="verify-icon" v-else />
-      <p>{{ message }}</p>
     </div>
   </div>
 </template>
@@ -11,38 +10,36 @@
 <script setup>
 import axios from 'axios'
 import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
 import { useToast } from 'vue-toastification'
 import VerifyIcon from '../icons/VerifyIcon.vue'
 import NotVerifyIcon from '../icons/NotVerifyIcon.vue'
-import { useRouter, useRoute } from 'vue-router'
-
 const SERVER_HOST = import.meta.env.VITE_SERVER_HOST
 
 const router = useRouter()
 const toast = useToast()
-const route = useRoute()
 const isVerified = ref(false)
-const token = ref(route.params.token)
 
 const verifyAccount = async () => {
   try {
-    const response = await axios.post(`${SERVER_HOST}/verify/${token.value}`)
-    isVerified.value = response.data.description
-    if(isVerified.value === true) {
-      toast.success('Account Verified')
-      setTimeout(() => router.push({ name: 'Login' }), 1000)
+    const response = await axios.post(`${SERVER_HOST}/auth/verify/${response.data.token}`);
+
+    if (response.data.message === "Account successfully verified") {
+      isVerified.value = !isVerified.value;
+      toast.success('Account Verified');
+      setTimeout(() => router.push({ name: 'Login' }), 1000);
     } else {
-      toast.error('Account Verified')
-      setTimeout(() => router.push({ name: 'Signin' }), 1000)
+    toast.error('Account not Verified');
+    setTimeout(() => router.push({ name: 'Signin' }), 1000);
     }
-
   } catch (error) {
-    toast.error(error.response.data.message)
+    toast.error(error.message);
   }
-}
+};
 
-
-onMounted(verifyAccount())
+onMounted(() => {
+  verifyAccount()
+})
 </script>
 
 <style>
