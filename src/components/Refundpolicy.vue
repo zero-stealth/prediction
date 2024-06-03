d078f17fcaefa82f39485f62f82df31eb17092a9
zero-stealth <penguincipher@gmail.com>
2024-06-03 18:10:04 +0300

    updated policy

--- src/components/Refundpolicy.vue
+++ src/components/Refundpolicy.vue
@@ -1,9 +1,23 @@
 <template>
- <div class="abt-info-p info-dn">
-      <h1>"Refund Policy</h1>
+  <div class="abt-info-p info-dn">
+    <h1>"Refund Policy</h1>
     <div class="abt-p-in">
       <p>
-        The information provided is intended solely for individuals or entities who are above the age of 18. sportypredict does not offer refunds for subscription payments and assumes no liability for any financial losses or gains. It is advised that individuals from countries where betting is illegal refrain from subscribing to our plans. For further details about SportyPredict, please refer to our comprehensive Terms and Conditions.
+        The information provided is intended solely for individuals or entities who are above the
+        age of 18. SportyPredict does not offer refunds for subscription payments and assumes no
+        liability for any financial losses or gains. It is advised that individuals from countries
+        where betting is illegal refrain from subscribing to our plans.
+      </p>
+      <p>
+        And if you wish cancel or stop from automatic subscriptions Kindly contact us not less than
+        3 days before the next payment date, then we’ll cancel or stop your
+        <a href="https://sportypredict.com/vip">VIP PLAN subscription</a>, Failing to inform us
+        earlier we shall not be able to refund the subscription fee and you’ll have to finish the
+        charged subscription period, kindly adhere to this.
+      </p>
+      <p>
+        For further details about SportyPredict, please refer to our comprehensive
+        <a href="https://sportypredict.com/terms&condition"> Terms and Conditions. </a>
       </p>
     </div>
   </div>
@@ -11,5 +25,3 @@
 <style>
 @import '../style/info.css';
 </style>
-
-
